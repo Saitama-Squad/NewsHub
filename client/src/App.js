027ab5b41829c04d3a9dd5e5b952c2e0409174bd
monieshravichandrran <monieshravichandrran@gmail.com>
2022-11-05 13:54:08 +0530

    added news feed page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { history } from "./history";
 import { Route, Router } from "react-router-dom";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import NewsFeed from "./pages/NewsFeed";
 
 const App = () => {
   
@@ -9,6 +10,7 @@ const App = () => {
     <Router history={history}>
       <Route path="/" exact component={Login} />
       <Route path="/profile" exact component={Profile}/>
+      <Route path="/news" exact component={NewsFeed} />
     </Router>
   );
 }
---
+++ client/src/component/News.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+
+
+const News = (props) => {
+  const hello = "hello"
+  const [liked, setLiked] = useState(false)
+  const [bookmark,setBookMark] = useState(false);
+
+  return (
+    <div className="mt-20">
+    <Card sx={{ minWidth: 200, maxWidth: 1000 }}>
+      <CardHeader
+        action={
+          <IconButton aria-label="settings">
+            { bookmark ? <BookmarkIcon onClick={()=>setBookMark(false)} style={{color: "green"}} /> : <BookmarkIcon onClick={()=>setBookMark(true)}/>}
+          </IconButton>
+        }
+        title={props.new.title}
+        subheader={props.new.date}
+      />
+      <CardMedia
+        component="img"
+        height="50"
+        style={{margin: "auto", height: "500px", width: "500px" }}
+        src={props.new.image}
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {props.new.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        {liked ?
+          <IconButton style={{ color: 'red' }} onClick={() => {
+            setLiked(false)
+          }} aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          :
+          <IconButton style={{ color: 'gray' }} onClick={() => {
+            setLiked(true)
+          }} aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+        }
+        <a href={`https://api.WhatsApp.com/send?text=` + props.new.article}>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+        </a>
+      </CardActions>
+    </Card>
+    </div>
+  );
+}
+
+export default News;
